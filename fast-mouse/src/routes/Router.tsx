b20862761390f75c42189e3b1d8d2130b1057079
Gabriella Costa <gabx2205@gmail.com>
2024-09-16 15:25:57 -0300

    feat: room page

--- fast-mouse/src/routes/Router.tsx
+++ fast-mouse/src/routes/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "../pages/Home"
+import Room from "../pages/Room"
 
 function Router() {
     return (
       <Routes>
           <Route path='/' element={<Home/>}></Route>
-          <Route path='/room/:id' element={<Home/>}></Route>
+          <Route path='/room' element={<Room/>}></Route>
       </Routes>
     )
   }
