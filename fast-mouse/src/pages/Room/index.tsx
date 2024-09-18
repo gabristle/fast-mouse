e6c69a6bf328bad08712b9f3aaebe87f50d3e6ed
Gabriella Costa <gabx2205@gmail.com>
2024-09-17 22:30:22 -0300

    feat: room page css

--- fast-mouse/src/pages/Room/index.tsx
+++ fast-mouse/src/pages/Room/index.tsx
@@ -1,24 +1,75 @@
 import styles from './style.module.css'
-import logo from '../../assets/logo.png'
-import bgTop from '../../assets/bg-top.png'
-import bgBottom from '../../assets/bg-bottom.png'
+import Layout from '../../layouts'
+import Button from '../../components/Button'
+import iconUser from '../../assets/icon-user.png'
+import iconOwner from '../../assets/icon-owner.png'
 
 function Room() {
   return (
-    <div>
-        <img src={bgTop} alt="top background" className={styles.topBg} />
-        <header className={styles.header}>
-            <img src={logo}></img>
-        </header>
-        <main>
-            <section>
+    <>
+      <Layout headerClass={styles.header}>
+        <div className={styles.body}>
+          <main className={styles.main}>
+            <aside className={styles.aside}>
+              <div className={styles.colleagues}>
+                <div className={`${styles.owner} ${styles.person}`}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <img src={iconOwner} alt="" className={styles.iconInfo}/>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+                <div className={styles.person}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <div className={styles.rank}>1</div>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+                <div className={styles.person}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <div className={styles.rank}>2</div>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+                <div className={styles.person}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <div className={styles.rank}>3</div>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+                <div className={styles.person}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <div className={styles.rank}>4</div>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+                <div className={styles.person}>
+                  <img src={iconUser} alt="" className={styles.iconUser}/>
+                  <div className={styles.rank}>5</div>
+                  <p>Gabriella Ribeiro</p>
+                </div>
+              </div>
+            </aside>
+            <section className={styles.section}>
+              <select name="" id="" className={styles.select}>
+                <option value="5" className={styles.option}>5 colleagues</option>
+                <option value="10" className={styles.option}>10 colleagues</option>
+                <option value="15" className={styles.option}>15 colleagues</option>
+                <option value="20" className={styles.option}>20 colleagues</option>
+                <option value="25" className={styles.option}>25 colleagues</option>
+                <option value="30" className={styles.option}>30 colleagues</option>
+                <option value="35" className={styles.option}>35 colleagues</option>
+                <option value="40" className={styles.option}>40 colleagues</option>
+              </select>
+              <h1 className={styles.howItWorks}>How it works?</h1>
+              <div className={styles.instructions}>
+                <p className={styles.text}><span>1. Share the Code:</span> After creating the room, share the code with your colleagues so they can join.</p>
+                <p className={styles.text}><span>2. Start the Turn:</span> Once everyone is in, click "Start Turn" to kick things off.</p>
+                <p className={styles.text}><span>3. Countdown:</span> A 3-second countdown will appear on everyone’s screen.</p>
+                <p className={styles.text}><span>4. Raise Hands:</span> After the countdown, your team will click to raise their hands.</p>
+                <p className={styles.text}><span>5. Rankings:</span> The list will automatically update based on who clicked first.</p>
+                <p className={styles.callToAction}>Easy, fast and fun - <span>let the race begin!</span></p>
+              </div>
+              <Button text={'START TURN'} className={styles.button}></Button>
             </section>
-        </main>
-        <footer className={styles.footer}>
-        <p>coded with love by <a href={'https://github.com/gabristle'} className={styles.link}>gabristle</a></p>
-        </footer>
-        <img src={bgBottom} alt="bottom background" className={styles.bottomBg} />
-    </div>
+          </main>
+        </div>
+      </Layout>
+    </>
   )
 }
 
